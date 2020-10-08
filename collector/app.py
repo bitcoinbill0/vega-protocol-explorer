0ee22d01e7d39b49de24d2afefe87298ffcb5935
Bitcoin Bill <bitcoinbill000@protonmail.com>
2020-10-08 20:37:49 +0100

    added subs for all data

--- collector/app.py
+++ collector/app.py
@@ -7,10 +7,10 @@ API_URL = os.environ['API_URL']
 WS_URL = os.environ['WS_URL']
 
 def get_markets():
-    return requests.get(f"{API_URL}/markets").json()["markets"][0:1]
+    return requests.get(f"{API_URL}/markets").json()["markets"]
 
 def get_parties():
-    return requests.get(f"{API_URL}/parties").json()["parties"][0:3]
+    return requests.get(f"{API_URL}/parties").json()["parties"]
 
 def get_positions_query(party_id):
     return '''
@@ -26,8 +26,8 @@ def get_positions_query(party_id):
                 averageEntryPrice
                 margins {
                     asset {
-                        id,
-                        name,
+                        id
+                        name
                         symbol
                     }
                     maintenanceLevel
@@ -82,90 +82,136 @@ def get_market_data_query(market_id):
         }
     '''
 
-# orders_query = '''
-#     subscription {
-#         orders(
-#             marketId: "''' + market_id + '''",
-#             partyId: "''' + party_id + '''"
-#         ) {
-#             id
-#             price
-#             timeInForce
-#             side
-#             market {
-#                 id
-#                 name
-#             }
-#             size
-#             remaining
-#             party {
-#                 id
-#             }
-#             createdAt
-#             expiresAt
-#             status
-#             reference
-#             trades {
-#                 buyOrder
-#                 sellOrder
-#                 buyer {
-#                     id
-#                 }
-#                 seller {
-#                     id
-#                 }
-#                 aggressor
-#                 price
-#                 size
-#                 createdAt
-#             }
-#             type
-#             rejectionReason
-#         }
-#     }
-# '''
+def get_orders_query(party_id, market_id):
+    orders_query = '''
+        subscription {
+            orders(
+                marketId: "''' + market_id + '''",
+                partyId: "''' + party_id + '''"
+            ) {
+                id
+                price
+                timeInForce
+                side
+                market {
+                    id
+                    name
+                }
+                size
+                remaining
+                party {
+                    id
+                }
+                createdAt
+                expiresAt
+                status
+                reference
+                trades {
+                    buyOrder
+                    sellOrder
+                    buyer {
+                        id
+                    }
+                    seller {
+                        id
+                    }
+                    aggressor
+                    price
+                    size
+                    createdAt
+                }
+                type
+                rejectionReason
+            }
+        }
+    '''
 
-# market_depth_query = '''
-#     subscription {
-#         marketDepth(marketId: "''' + market_id + '''") {
-#             market {
-#             id
-#                 name
-#             }
-#             buy {
-#                 price
-#                 volume
-#                 numberOfOrders
-#                 cumulativeVolume
-#             }
-#             sell {
-#                 price
-#                 volume
-#                 numberOfOrders
-#                 cumulativeVolume
-#             }
-#         }
-#     }
-# '''
+def get_market_depth_query(market_id):
+    return '''
+        subscription {
+            marketDepth(marketId: "''' + market_id + '''") {
+                market {
+                id
+                    name
+                }
+                buy {
+                    price
+                    volume
+                    numberOfOrders
+                }
+                sell {
+                    price
+                    volume
+                    numberOfOrders
+                }
+            }
+        }
+    '''
 
-# margins_query = '''
-#     subscription {
-#         margins(
-#             partyId: "''' + party_id + '''",
-#             marketID: "''' + market_id + '''"
-#         ) {
-#             asset
-#             maintenanceLevel
-#             searchLevel
-#             initialLevel
-#             collateralReleaseLevel
-#             timestamp
-#             party {
-#                 id
-#             }
-#         }
-#     }
-# '''
+def get_margins_query(party_id, market_id):
+    return '''
+        subscription {
+            margins(
+                partyId: "''' + party_id + '''",
+                marketID: "''' + market_id + '''"
+            ) {
+                asset {
+                    id
+                    name
+                    symbol
+                }
+                maintenanceLevel
+                searchLevel
+                initialLevel
+                collateralReleaseLevel
+                timestamp
+                party {
+                    id
+                }
+            }
+        }
+    '''
+
+def get_trades_query(party_id, market_id):
+    return '''
+        subscription {
+            trades(
+                partyId: "''' + party_id + '''",
+                marketId: "''' + market_id + '''"
+            ) {
+                id
+                market {
+                    id
+                    name
+                }
+                buyOrder
+                sellOrder
+                buyer {
+                    id
+                }
+                seller {
+                    id
+                }
+                aggressor
+                price
+                type
+                size
+                createdAt
+                buyerFee {
+                    makerFee
+                    infrastructureFee
+                    liquidityFee
+                }
+                sellerFee {
+                    makerFee
+                    infrastructureFee
+                    liquidityFee
+                }
+                buyerAuctionBatch
+                sellerAuctionBatch
+            }
+        }
+    '''
 
 def sub_init(ws):
     ws.send(json.dumps({
@@ -192,8 +238,45 @@ def sub_positions(ws):
             'payload': { 'query': get_positions_query(party_id) }
         }))
 
+def sub_trades(ws):
+    parties = get_parties()
+    markets = get_markets()
+    for party in parties:
+        for market in markets:
+            party_id = party['id']
+            market_id = market['id']
+            ws.send(json.dumps({
+                'id': f'trade_party_{party_id}_market_{market_id}',
+                'type': 'start',
+                'payload': { 'query': get_trades_query(party_id, market_id) }
+            }))
+
+def sub_margins(ws):
+    parties = get_parties()
+    markets = get_markets()
+    for party in parties:
+        for market in markets:
+            party_id = party['id']
+            market_id = market['id']
+            ws.send(json.dumps({
+                'id': f'margin_party_{party_id}_market_{market_id}',
+                'type': 'start',
+                'payload': { 'query': get_margins_query(party_id, market_id) }
+            }))
+
+def sub_market_depth(ws):
+    markets = get_markets()
+    for market in markets:
+        market_id = market['id']
+        ws.send(json.dumps({
+            'id': f'market_depth_{market_id}',
+            'type': 'start',
+            'payload': { 'query': get_market_depth_query(market_id) }
+        }))
+
 def on_message(ws, message):
-    print(json.loads(message))
+    # TODO - parse message and store data 
+    pass
 
 def on_error(ws, error):
     print(error)
@@ -205,6 +288,9 @@ def on_open(ws):
     sub_init(ws)
     sub_markets(ws)
     sub_positions(ws)
+    sub_margins(ws)
+    sub_market_depth(ws)
+    sub_trades(ws)
 
 def connect():
     ws = websocket.WebSocketApp(f"{WS_URL}/query",
